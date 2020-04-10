0e5eaa3cc96a0bba82d05708531c36fc6f97067c
Siva Sivaraman <60401095+siva-msft@users.noreply.github.com>
2020-04-10 14:37:44 -0700

    Adding first example file

---
+++ test_example.cpp
@@ -0,0 +1,14 @@
+extern "C" {
+    void* memset(void*, int, size_t);
+    int memcmp(const void*, const void*, size_t);
+    void* malloc(size_t);
+}
+
+struct T1 {
+    T1 *t1pt1;
+};
+
+int main() {
+    T1 t1, *pt1 = &t1;
+    memset(&t1, 0, sizeof(&t1)); // recommend: sizeof(struct T1)
+}
