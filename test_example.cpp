f7a5d130d0108a9bd00f8407929fed3c2e366ec1
Siva Sivaraman <60401095+siva-msft@users.noreply.github.com>
2020-04-14 09:51:05 -0700

    version2

--- test_example.cpp
+++ test_example.cpp
@@ -8,7 +8,14 @@ struct T1 {
     T1 *t1pt1;
 };
 
+void foo() {
+    T1 t1, *pt1 = &t1;
+    memset(&t1, 0, sizeof(t1)); // correct usage
+    memset(&t1, 0, sizeof(&t1)); // recommend: sizeof(struct T1) or sizeof(t1)
+}
+
 int main() {
     T1 t1, *pt1 = &t1;
     memset(&t1, 0, sizeof(&t1)); // recommend: sizeof(struct T1)
+    foo();
 }
